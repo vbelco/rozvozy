f820cf2f3e032ab99b3d228663c36d0c9d2cfe2e
Viktor Belko <vbelco@gmail.com>
2021-12-22 09:57:24 +0100

    upravena navigacia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,19 +3,21 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
     </head>
     <body>
         <title>Rozvoz</title>
-            <div>
+            <div class="navigacia">
+                <ul class="nav">
                 @foreach ($rozvozy as $rozvoz_smer)
-                    <div>
-                    <a href="{{route('rozvoz', ['rozvoz_id' => $rozvoz_smer->orders_status_id ]) }}" >
-                        {{ $rozvoz_smer->orders_status_name }}
-                    </a>
-                    </div>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('rozvoz', ['rozvoz_id' => $rozvoz_smer->orders_status_id ]) }}" >
+                            {{ $rozvoz_smer->orders_status_name }}
+                        </a>
+                    <li>
                 @endforeach
+                </ul>
             </div>
 
             <div>
